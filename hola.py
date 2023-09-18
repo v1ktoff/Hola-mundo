70b77de7cb8b5f8d475a5ec4bd496d3f93c388c5
Victor Gabriel <vgarredo@outlook.com>
2023-09-18 13:16:18 -0600

    My second commit

---
+++ hola.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# F-society v1ktoff
+# License GNU/GPL
+# 2023/09/18
+# SOlo Dios y yo sabaimos lo que hacia este codigo, ahora solo dios sabe
+def hola():
+	print("Hola mundo")
+hola()
